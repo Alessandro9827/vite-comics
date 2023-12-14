76a46837bf62e086252757d616b4f142c8ee64df
Alessandro9827 <ale.moramarco27@gmail.com>
2023-12-14 20:29:26 +0100

    added folder img

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <div class="nav-container">
+        <img src="./assets/" alt="">
+        <ul>
+           <li></li>
+           <li></li>
+           <li></li>
+           <li></li>
+           <li></li>
+           <li></li>
+           <li></li>
+           <li></li>
+           <li></li>
+           <li></li>
+        </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
