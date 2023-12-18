b83207535ffb541af7ed8ee88925d0b7b29415bf
Alessandro9827 <ale.moramarco27@gmail.com>
2023-12-18 21:03:02 +0100

    added cardList array

---
+++ src/components/CardElement.vue
@@ -0,0 +1,31 @@
+<template>
+  <article>
+    <img :src="imageSrc" alt="">
+    <p>
+        {{ serie }}
+    </p>
+  </article>
+</template>
+
+<script>
+export default {
+    name: 'PastaElement',
+    // props: [
+    //     'imageSrc',
+    //     'title',
+    //     'type',
+    //     'cookingTime'
+    // ]
+    props: {
+        imageSrc : {
+            type: String,
+            required: true
+        },
+        serie : String,
+    }
+}
+</script>
+
+<style>
+
+</style>
